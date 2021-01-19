42bffba1343363d7c089b30246c6c459b2383067
Dagmara <przygocka.dagmara@gmail.com>
2021-01-19 16:57:12 +0100

    build war insted jar

--- src/main/java/server/sport/SportApplication.java
+++ src/main/java/server/sport/SportApplication.java
@@ -2,12 +2,19 @@ package server.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SportApplication {
+public class SportApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SportApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SportApplication.class, args);
     }
+
 
 }
