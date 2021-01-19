b4ff384a4d3b133064bedb05f492e762ab21ac6d
Dagmara <przygocka.dagmara@gmail.com>
2021-01-19 16:57:12 +0100

    Revert "build war insted jar"
    This reverts commit 42bffba1343363d7c089b30246c6c459b2383067.

--- src/main/java/server/sport/SportApplication.java
+++ src/main/java/server/sport/SportApplication.java
@@ -2,19 +2,12 @@ package server.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SportApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SportApplication.class);
-    }
+public class SportApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SportApplication.class, args);
     }
-
 
 }
